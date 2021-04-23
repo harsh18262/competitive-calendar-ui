0538170fbd56f0cd3d50c10aede264cca197ef68
Shashwat Sharma <shashwat2910@gmail.com>
2021-04-23 21:41:53 +0530

    main file updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:competitive_calender/screens/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,6 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
   }
 }
